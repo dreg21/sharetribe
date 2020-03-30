3b70de49ab6f31ed586564c985a8957d46a48528
Aivils Štoss <aivils@ithouse.lv>
2020-03-30 11:08:44 +0300

    [Harmony removal] atomic transaction

--- app/services/transaction_service/process/preauthorize.rb
+++ app/services/transaction_service/process/preauthorize.rb
@@ -68,43 +68,27 @@ module TransactionService::Process
 
     def do_finalize_create(transaction_id, community_id)
       tx = TxStore.get_in_community(community_id: community_id, transaction_id: transaction_id)
-      gateway_adapter = TransactionService::Transaction.gateway_adapter(tx.payment_gateway)
 
       res =
         if tx.current_state == :preauthorized
           Result::Success.new()
         else
-          booking_res =
-            if tx.availability.to_sym == :booking && !tx.booking.valid?
-              void_payment(gateway_adapter, tx)
-              Result::Error.new(TransactionService::Transaction::BookingDatesInvalid.new(I18n.t("error_messages.booking.double_booking_payment_voided")))
-            else
-              Result::Success.new()
-            end
-
-          booking_res.on_success {
-            if tx.stripe_payments.last.try(:intent_requires_action?)
-              TransactionService::StateMachine.transition_to(tx.id, :payment_intent_requires_action)
-            else
-              TransactionService::StateMachine.transition_to(tx.id, :preauthorized)
-            end
-          }
+          new_state = tx.stripe_payments.last.try(:intent_requires_action?) ? :payment_intent_requires_action : :preauthorized
+          transition_tx = TransactionService::StateMachine.transition_to(tx.id, new_state)
+          if transition_tx && transition_tx.current_state.to_sym == new_state
+            Result::Success.new()
+          elsif transition_tx && transition_tx.booking && transition_tx.booking.errors.any?
+            Result::Error.new(
+              TransactionService::Transaction::BookingDatesInvalid.new(
+                I18n.t("error_messages.booking.double_booking_payment_voided")))
+          else
+            Result::Error.new('Generic payment error')
+          end
         end
 
       res.and_then {
         Result::Success.new(TransactionService::Transaction.create_transaction_response(tx))
       }
-    end
-
-    def void_payment(gateway_adapter, tx)
-      void_res = gateway_adapter.reject_payment(tx: tx, reason: "")[:response]
-
-      void_res.on_success {
-        logger.info("Payment voided after failed transaction", :void_payment, tx.slice(:community_id, :id))
-      }.on_error { |payment_error_msg, payment_data|
-        logger.error("Failed to void payment after failed booking", :failed_void_payment, tx.slice(:community_id, :id).merge(error_msg: payment_error_msg))
-      }
-      void_res
     end
 
     def reject(tx:, message:, sender_id:, gateway_adapter:)
--- app/services/transaction_service/state_machine.rb
+++ app/services/transaction_service/state_machine.rb
@@ -1,8 +1,4 @@
 module TransactionService
-  TransactionModel = ::Transaction
-
-  class BookingStateChangeError < StandardError
-  end
 
   TransitionMetadata= EntityUtils.define_builder(
     [:paypal_pending_reason, :symbol],
@@ -14,93 +10,29 @@ module TransactionService
   module StateMachine
     module_function
 
+    # returns nil if cannot do transition
+    # returns transaction in case of successful transition
+    # returns transaction with errors in case of failed transition
     def transition_to(transaction_id, new_status, metadata = nil)
       new_status = new_status.to_sym
-
-      if can_transition_to?(transaction_id, new_status)
-        transaction = TransactionModel.where(id: transaction_id, deleted: false).first
-        old_status = transaction.current_state.to_sym if transaction.current_state.present?
-        payment_type = transaction.payment_gateway.to_sym
-
-        handle_transition(transaction, payment_type, old_status, new_status)
-
-        save_transition(transaction, new_status, metadata)
-      end
-    end
-
-    def save_transition(transaction, new_status, metadata = nil)
-      transaction.current_state = new_status
-      transaction.save!
-
       metadata_hash = Maybe(metadata)
         .map { |data| TransitionMetadata.call(data) }
         .map { |data| HashUtils.compact(data) }
         .or_else(nil)
-
+      transaction = ::Transaction.exist.find_by(id: transaction_id)
       state_machine = TransactionProcessStateMachine.new(transaction, transition_class: TransactionTransition)
-      state_machine.transition_to!(new_status, metadata_hash)
 
-      transaction.touch(:last_transition_at) # rubocop:disable Rails/SkipsModelValidations
-
-      transaction.reload
+      if transaction && state_machine.can_transition_to?(new_status)
+        state_machine.transition_to(new_status, metadata_hash)
+        transaction
+      end
     end
 
     def can_transition_to?(transaction_id, new_status)
-      transaction = TransactionModel.where(id: transaction_id, deleted: false).first
-      if transaction
-        state_machine = TransactionProcessStateMachine.new(transaction, transition_class: TransactionTransition)
-        state_machine.can_transition_to?(new_status)
-      end
-    end
+      transaction = ::Transaction.exist.find_by(id: transaction_id)
+      state_machine = TransactionProcessStateMachine.new(transaction, transition_class: TransactionTransition)
 
-    def handle_transition(transaction, payment_type, old_status, new_status)
-      case new_status
-      when :preauthorized
-        preauthorized(transaction, payment_type)
-      end
-    end
-
-    # privates
-
-    def preauthorized(transaction, payment_type)
-      expiration_period = TransactionService::Transaction.authorization_expiration_period(payment_type)
-      gateway_expires_at = case payment_type
-                           when :paypal
-                             # expiration period in PayPal is an estimate,
-                             # which should be quite accurate. We can get
-                             # the exact time from Paypal through IPN notification. In this case,
-                             # we take the 3 days estimate and add 10 minute buffer
-                             expiration_period.days.from_now - 10.minutes
-                           when :stripe
-                             expiration_period.days.from_now - 10.minutes
-                           else
-                             raise ArgumentError.new("Unknown payment_type: '#{payment_type}'")
-                           end
-
-      booking_ends_on = transaction.booking&.final_end
-      expire_at = TransactionService::Transaction.preauth_expires_at(gateway_expires_at, booking_ends_on)
-
-      Delayed::Job.enqueue(TransactionPreauthorizedJob.new(transaction.id), priority: 5)
-      Delayed::Job.enqueue(AutomaticallyRejectPreauthorizedTransactionJob.new(transaction.id), priority: 8, run_at: expire_at)
-
-      setup_preauthorize_reminder(transaction.id, expire_at)
-    end
-
-    # "private" helpers
-
-    def setup_preauthorize_reminder(transaction_id, expire_at)
-      reminder_days_before = 1
-
-      reminder_at = expire_at - reminder_days_before.day
-      send_reminder = reminder_at > Time.zone.now
-
-      if send_reminder
-        Delayed::Job.enqueue(TransactionPreauthorizedReminderJob.new(transaction_id), priority: 9, :run_at => reminder_at)
-      end
-    end
-
-    def logger
-      SharetribeLogger.new(:transaction_transition_events)
+      transaction && state_machine.can_transition_to?(new_status)
     end
   end
 end
